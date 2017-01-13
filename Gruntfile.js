7c4db174e358f2994b3438070628afb1a21304a5
Prateek Saxena <prtksxna@gmail.com>
2017-01-13 12:14:44 +0530

    Add version based on OOjs UI version

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,13 @@ module.exports = function( grunt ) {
 grunt.loadNpmTasks( 'grunt-webfont' );
 
 grunt.initConfig( {
+	pkg: grunt.file.readJSON( 'package.json' ),
 	webfont: {
 		icons: {
 			src: 'node_modules/oojs-ui/src/themes/mediawiki/images/icons/*.svg',
 			dest: '.',
 			options: {
+				version: '<%=pkg.devDependencies[ "oojs-ui" ]%>',
 				font: 'WikimediaIcons',
 				fontFilename: 'wikimedia-icons',
 				fontFamilyName: 'Wikimedia Icons',
